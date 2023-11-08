bfcd0a4425211898d7b22f3e6b6b1bf893dbc5cd
AzizVorrez <azizvorrez8@gmail.com>
2023-11-08 13:28:47 +0100

    req.body is empty : RESOLVED

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const UserSchema = mongoose.Schema({
   first_name: { type: String, required: true },
   last_name: { type: String, required: true },
   phone_number: { type: String, required: true },
-  password: { type: Number, required: true },
+  password: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
