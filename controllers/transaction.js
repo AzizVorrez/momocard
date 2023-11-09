dee2123655c46c1159462f5a8fe73d540ce137ad
AzizVorrez <azizvorrez8@gmail.com>
2023-11-09 23:38:33 +0100

    Creating Token automatically !

--- controllers/transaction.js
+++ controllers/transaction.js
@@ -1,4 +1,98 @@
-exports.refill = (req, res, next) => {};
+const Transaction = require("../models/Transaction");
+const User = require("../models/User");
+const uuid = require("uuid");
+
+const externalTransactionId = uuid.v4();
+
+const autorization = (genereToken) => {
+  // Request body
+  const body = {
+    providerCallbackHost: "MoMoCard",
+  };
+
+  fetch("https://sandbox.momodeveloper.mtn.com/v1_0/apiuser", {
+    method: "POST",
+    body: JSON.stringify(body),
+    // Request headers
+    headers: {
+      "X-Reference-Id": externalTransactionId,
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache",
+      "Ocp-Apim-Subscription-Key": "078933bfe87647b0a49024c377d1c468",
+    },
+  })
+    .then((response) => {
+      if (response.status != 201) {
+        response.status(500).json({ message: "Internal Error !" });
+      } else {
+        fetch(
+          `https://sandbox.momodeveloper.mtn.com/v1_0/apiuser/${externalTransactionId}/apikey`,
+          {
+            method: "POST",
+            // Request headers
+            headers: {
+              "Cache-Control": "no-cache",
+              "Ocp-Apim-Subscription-Key": "078933bfe87647b0a49024c377d1c468",
+            },
+          }
+        )
+          .then((response) => {
+            console.log(response.status);
+            console.log(response.text());
+          })
+          .catch((err) => console.error(err));
+      }
+    })
+    .catch((err) => console.error(err));
+};
+
+exports.refill = (req, res, next) => {
+  const currency = "EUR";
+  const serviceProviderUserName = "MoMoCard";
+  const SubscriptionKey = "078933bfe87647b0a49024c377d1c468";
+
+  User.findById(req.body.user)
+    .then((user) => {
+      if (!user) {
+        res.status(400).json({ message: "L'utilisateur est introuvable" });
+      } else {
+        const body = {
+          externalTransactionId: externalTransactionId,
+          money: {
+            amount: req.body.amount,
+            currency: currency,
+          },
+          customerReference: req.body.partyId,
+          serviceProviderUserName: "MoMoCard",
+        };
+        console.log(body);
+        fetch("https://sandbox.momodeveloper.mtn.com/collection/v2_0/payment", {
+          method: "POST",
+          body: JSON.stringify(body),
+
+          // Request headers
+          headers: {
+            Authorization: autorization,
+            "X-Reference-Id": externalTransactionId,
+            "X-Target-Environment": "sandbox",
+            "Content-Type": "application/json",
+            "Cache-Control": "no-cache",
+            "Ocp-Apim-Subscription-Key": SubscriptionKey,
+          },
+        })
+          .then((response) => {
+            res.status(200).json({ response: response.text() });
+            console.log(response.status);
+            console.log(response.text());
+          })
+          .catch((err) => console.error(err));
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).json({ error });
+    });
+};
 
 exports.receive = (req, res, next) => {};
 
--- models/Transaction.js
+++ models/Transaction.js
@@ -1,7 +1,15 @@
 const mongoose = require("mongoose");
 
 const TransactionSchema = mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  amount: { type: Number, required: true },
+  currency: { type: String, required: true },
+  externalId: { type: String, required: true },
+  partyIdType: { type: String, required: true },
+  partyId: { type: String, required: true },
+  payerMessage: { type: String, required: true },
+  payeeNote: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
-mondule.exports = mongoose.model("Transaction", TransactionSchema);
+module.exports = mongoose.model("Transaction", TransactionSchema);
